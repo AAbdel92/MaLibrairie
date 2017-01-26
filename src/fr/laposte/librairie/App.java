5d1e7882429dd781a667c5ec3521f687611f586a
AAbdel92 <ahmar.abdel@gmail.com>
2017-01-26 10:39:45 +0100

    first commit

---
+++ src/fr/laposte/librairie/App.java
@@ -0,0 +1,13 @@
+package fr.laposte.librairie;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Librairie librairie = new Librairie();
+		librairie.constituerStock();
+		librairie.afficherStock();
+
+	} 
+
+}
---
+++ src/fr/laposte/librairie/Livre.java
@@ -0,0 +1,22 @@
+package fr.laposte.librairie;
+
+public class Livre {
+	private String nom;
+	private String auteur;
+	private String isbn;
+	private float prix;
+	
+	public Livre(String nom, String auteur, String isbn, float prix) {
+		super();
+		this.nom = nom;
+		this.auteur = auteur;
+		this.isbn = isbn;
+		this.prix = prix;
+	}
+	
+	public void sePresenter() {
+		String moi = "Livre : " + this.nom + " , " + this.auteur;
+		System.out.println(moi);
+	}
+
+}
